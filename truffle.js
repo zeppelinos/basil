dd94936899f088b1b57d17446aee34acd34af10b
Alejandro Santander <Palebluedot@gmail.com>
2018-03-27 16:37:23 -0300

    Add 2nd migration script

--- truffle.js
+++ truffle.js
@@ -3,6 +3,11 @@ require('babel-polyfill');
 
 module.exports = {
   networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*', // eslint-disable-line camelcase
+    },
     ropsten: {
       host: 'localhost',
       port: 8545,

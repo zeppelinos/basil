283ab4635ff222b6a40571734c307497aee5f7d3
Alejandro Santander <Palebluedot@gmail.com>
2018-03-28 23:59:28 -0300

    Log info on 2nd migration

--- migrations/2_deploy_basil.js
+++ migrations/2_deploy_basil.js
@@ -7,20 +7,32 @@ const abi = require('ethereumjs-abi')
 module.exports = async function(deployer, network, accounts) {
 
   // Deploy the upgradeability wrappers.
+  console.log(`> deploying registry`);
   const registry = await Registry.new();
+  console.log(`> registry deployed: ${registry.address}`);
+  console.log(`> deploying factory`);
   const factory = await Factory.new(registry.address);
+  console.log(`> factory deployed: ${registry.address}, with registry: ${await factory.registry()}`);
 
   // Deploy basil's first implementation and register it.
+  const version = '0';
+  console.log(`> deploying Basil implementation version ${version}`);
   const implementation = await Basil.new();
-  await registry.addVersion('0', implementation.address);
+  console.log(`> implementation deployed: ${implementation.address}`);
+  await registry.addVersion(version, implementation.address);
+  console.log(`> registered version ${version}, implementation: ${await registry.getVersion('version')}`);
 
   // Create the proxy with the first implementation, and execute
   // the implementation's initialze function.
   const owner = accounts[0];
-  const methodId = abi.methodID('initialize', ['address']).toString('hex');
-  const params = abi.rawEncode(['address'], [owner]).toString('hex');
-  const initializeData = '0x' + methodId + params;
+  const initializeData = implementation.contract.initialize.getData(owner);
+  console.log(`> creating proxy with owner ${owner}, and call data: ${initializeData}`);
   const proxyData = await factory.createProxyAndCall('0', initializeData, { from: owner })
+  console.log(`> proxyData: ${proxyData}`);
   const proxyAddress = proxyData.logs[0].args.proxy;
-  await Basil.at(proxyAddress)
+  console.log(`> proxy deployed: ${proxyAddress}`);
+
+  // Verify proxy.
+  const basil_v0 = await Basil.at(proxyAddress);
+  console.log(`> proxy owner: ${await basil_v0.owner()}`);
 };

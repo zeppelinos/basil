0c40e770dd3753b635e347bef728e482b3914a46
Alejandro Santander <Palebluedot@gmail.com>
2018-03-29 01:47:57 -0300

    Added colors to log information during deployment

--- migrations/2_deploy_basil.js
+++ migrations/2_deploy_basil.js
@@ -2,6 +2,7 @@ const Registry = artifacts.require('zos-core/contracts/Registry.sol');
 const Factory = artifacts.require('zos-core/contracts/Factory.sol');
 const Basil = artifacts.require("./Basil.sol");
 
+const colors = require('colors');
 const fs = require('fs');
 const data = require('../data/deploy_data.json');
 
@@ -9,43 +10,43 @@ module.exports = function(deployer, network, accounts) {
   deployer.then(async () => {
 
     // Deploy the version registry.
-    console.log(`> deploying registry`);
+    console.log(colors.cyan(`> deploying registry`));
     const registry = await Registry.new();
-    console.log(`> registry deployed: ${registry.address}`);
+    console.log(colors.gray(`> registry deployed: ${registry.address}`));
 
     // Deploy the proxy factory.
-    console.log(`> deploying factory`);       
+    console.log(colors.cyan(`> deploying factory`));       
     const factory = await Factory.new(registry.address);
-    console.log(`> factory deployed: ${registry.address}, with registry: ${await factory.registry()}`);
+    console.log(colors.gray(`> factory deployed: ${registry.address}, with registry: ${await factory.registry()}`));
                                               
     // Deploy basil's first implementation and register it.
     const version = '0';                      
-    console.log(`> deploying Basil implementation version ${version}`);
+    console.log(colors.cyan(`> deploying Basil implementation version ${version}`));
     const implementation = await Basil.new();
-    console.log(`> implementation deployed: ${implementation.address}`);
+    console.log(colors.gray(`> implementation deployed: ${implementation.address}`));
     await registry.addVersion(version, implementation.address);
-    console.log(`> registered version ${version}, implementation: ${await registry.getVersion(version)}`);
+    console.log(colors.gray(`> registered version ${version}, implementation: ${await registry.getVersion(version)}`));
                                                       
     // Create the proxy with the first implementation, and execute
     // the implementation's initialze function.       
     const owner = accounts[0];                        
     const initializeData = implementation.contract.initialize.getData(owner);
-    console.log(`> creating proxy with owner ${owner}, and call data: ${initializeData}`);
+    console.log(colors.cyan(`> creating proxy with owner ${owner}, and call data: ${initializeData}`));
     const proxyData = await factory.createProxyAndCall('0', initializeData, { from: owner })
-    console.log(`> proxy creation tx: ${proxyData.tx}`);
+    console.log(colors.gray(`> proxy creation tx: ${proxyData.tx}`));
     const proxyAddress = proxyData.logs[0].args.proxy;
-    console.log(`> proxy deployed: ${proxyAddress}`);
+    console.log(colors.gray(`> proxy deployed: ${proxyAddress}`));
                                         
     // Verify proxy.                    
     const basil_v0 = await Basil.at(proxyAddress);
-    console.log(`> proxy owner: ${await basil_v0.owner()}`);
+    console.log(colors.gray(`> proxy owner: ${await basil_v0.owner()}`));
                                         
     // Store proxy data for selected network.
     data[network].proxyAddress = proxyAddress;
     data[network].factoryAddress = factory.address;
     data[network].registryAddress = registry.address;
     const writeData = JSON.stringify(data, null, 2);
-    console.log(`> storing deploy data: ${writeData}`);
+    console.log(colors.green(`> storing deploy data: ${writeData}`));
     fs.writeFileSync('./data/deploy_data.json', writeData, 'utf8')
   });
 };
--- migrations/3_update_basil.js
+++ migrations/3_update_basil.js
@@ -3,36 +3,37 @@ const OwnedUpgradeabilityProxy = artifacts.require('zos-core/contracts/upgradeab
 const Basil = artifacts.require("./Basil.sol");
 
 const data = require('../data/deploy_data.json');
+const colors = require('colors');
 
 module.exports = async function(deployer, network, accounts) {
   deployer.then(async () => {
     // Retrieve proxy.                    
     const proxyAddress = data[network].proxyAddress;
     if(!proxyAddress) return;             
-    console.log(`> retrieving proxy at: ${proxyAddress}`);
+    console.log(colors.gray(`> retrieving proxy at: ${proxyAddress}`));
     const proxy = OwnedUpgradeabilityProxy.at(proxyAddress);
 
     // Retrieve registry.
     const registryAddress = data[network].registryAddress;
     if(!registryAddress) return;             
-    console.log(`> retrieving registry at: ${registryAddress}`);
+    console.log(colors.gray(`> retrieving registry at: ${registryAddress}`));
     const registry = Registry.at(registryAddress);
 
     // Deploy the new implementation.
     const version = '1';
-    console.log(`> deploying Basil implementation version ${version}`);
+    console.log(colors.cyan(`> deploying Basil implementation version ${version}`));
     const implementation = await Basil.new();
-    console.log(`> implementation deployed: ${implementation.address}`);
+    console.log(colors.gray(`> implementation deployed: ${implementation.address}`));
 
     // Upload new implementation to the registry.
     await registry.addVersion(version, implementation.address);
-    console.log(`> registered version ${version}, implementation: ${await registry.getVersion(version)}`);
+    console.log(colors.gray(`> registered version ${version}, implementation: ${await registry.getVersion(version)}`));
 
     // Update proxy to use this new version.
     const owner = accounts[0];                        
-    console.log(`> upgrading proxy to version ${version}`);
+    console.log(colors.gray(`> upgrading proxy to version ${version}`));
     await proxy.upgradeTo(version);
-    console.log(`> proxy upgraded to version ${await proxy.version()}, with implementation: ${await proxy.implementation()}`);
+    console.log(colors.cyan(`> proxy upgraded to version ${await proxy.version()}, with implementation: ${await proxy.implementation()}`));
   });
 }
 

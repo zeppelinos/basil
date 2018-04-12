289524aa355b2cbb44236ca44bd769f7e0032a07
Alejandro Santander <Palebluedot@gmail.com>
2018-04-12 10:56:09 -0300

    Greatly simplified tests using deployer

--- test/ZOS.test.js
+++ test/ZOS.test.js
@@ -1,11 +1,11 @@
 'use strict';
 
+import Deployer from 'kernel/deploy/objects/Deployer';
+
 const decodeLogs = require('zos-core/test/helpers/decodeLogs')
 
 const Basil = artifacts.require('Basil')
 const BasilTestUpgrade = artifacts.require('BasilTestUpgrade.sol')
-const Registry = artifacts.require('zos-core/contracts/Registry.sol')
-const UpgradeabilityProxyFactory = artifacts.require('zos-core/contracts/upgradeability/UpgradeabilityProxyFactory.sol')
 const ProjectController = artifacts.require('zos-core/contracts/ProjectController')
 const OwnedUpgradeabilityProxy = artifacts.require("zos-core/contracts/upgradeability/OwnedUpgradeabilityProxy.sol");
 
@@ -17,31 +17,17 @@ contract('ZOS', ([_, proxyOwner, owner, aWallet, someone, anotherone]) => {
     const contractName = 'Basil';
 
     before(async function () {
-      this.registry = await Registry.new();
-      this.factory = await UpgradeabilityProxyFactory.new();
-      this.fallbackProvider = {address: "0x0"}; // TODO: A deployed version of zos should go here.
-      this.controller = await ProjectController.new(projectName, this.registry.address, this.factory.address, this.fallbackProvider.address);
+      this.controller = await Deployer.projectController(proxyOwner, projectName);
     });
 
     describe('registry', function() {
 
-      it('is the assigned registry', async function() {
-        const registry = await this.controller.registry();
-        assert.equal(registry, this.registry.address);
-      });
-
       it('can add implementation 0', async function() {
-        this.basilImplementation0 = await Basil.new();
-        await this.registry.addImplementation('0', contractName, this.basilImplementation0.address);
-        const registryImplementation = await this.registry.getImplementation('0', contractName);
-        assert.equal(registryImplementation, this.basilImplementation0.address);
+        this.basilImplementation0 = await Deployer.deployAndRegister(this.controller, Basil, contractName, '0');
       });
 
       it('can add implementation 1', async function() {
-        this.basilImplementation1 = await BasilTestUpgrade.new();
-        await this.registry.addImplementation('1', contractName, this.basilImplementation1.address);
-        const registryImplementation = await this.registry.getImplementation('1', contractName);
-        assert.equal(registryImplementation, this.basilImplementation1.address);
+        this.basilImplementation1 = await Deployer.deployAndRegister(this.controller, BasilTestUpgrade, contractName, '1');
       });
       
       describe('controller', function() {
@@ -57,17 +43,22 @@ contract('ZOS', ([_, proxyOwner, owner, aWallet, someone, anotherone]) => {
         });
 
         it('can create a proxy for implementation 0', async function() {
-          const initData = this.basilImplementation0.contract.initialize.getData(owner);
-          const { receipt } = await this.controller.createAndCall(projectName, '0', contractName, initData);
-          const logs = decodeLogs([receipt.logs[0]], UpgradeabilityProxyFactory, this.factory.address);
-          const proxyAddress = logs.find(l => l.event === 'ProxyCreated').args.proxy
-          this.basilProxy = await OwnedUpgradeabilityProxy.at(proxyAddress)
-          assert.equal(await this.basilProxy.implementation(), this.basilImplementation0.address);
+          this.basilProxy = await Deployer.createProxyAndCall(
+            this.controller,
+            proxyOwner,
+            Basil,
+            contractName,
+            projectName,
+            '0',
+            ['address'],
+            proxyOwner
+          );
         });
 
         it('can upgrade the proxy to implementation 1', async function() {
-          await this.controller.upgradeTo(this.basilProxy.address, projectName, '1', contractName);
-          assert.equal(await this.basilProxy.implementation(), this.basilImplementation1.address);
+          await this.controller.upgradeTo(this.basilProxy.address, projectName, '1', contractName, {from: proxyOwner});
+          const proxy = OwnedUpgradeabilityProxy.at(this.basilProxy.address);
+          assert.equal(await proxy.implementation(), this.basilImplementation1.address);
           const basil_v1 = await BasilTestUpgrade.at(this.basilProxy.address);
           const msg = await basil_v1.sayHi();
           assert.equal(msg, "Hi!");

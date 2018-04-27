1a6eb8f8adde2f68a9ffc984d25602e830444df2
Alejandro Santander <Palebluedot@gmail.com>
2018-04-27 13:06:42 -0300

    Forgot to add file from last commit

---
+++ test/BasilWithTokens.behavior.js
@@ -0,0 +1,58 @@
+const assertRevert = require('./helpers/assertRevert.js');
+const should = require('chai').should();
+const shouldBehaveLikeBasil = require('./Basil.behavior.js');
+
+module.exports = function() {
+
+  shouldBehaveLikeBasil();
+
+  describe('token', function() {
+
+    it('is owned by the contract', async function() {
+      (await this.token.owner()).should.be.eq(this.basil.address);
+    });
+
+    it('has the correct token name', async function() {
+      (await this.token.name()).should.be.eq(this.tokenName);
+    });
+
+    it('has the correct token symbol', async function() {
+      (await this.token.symbol()).should.be.eq(this.tokenSymbol);
+    });
+
+    it('cannot be set a second time', async function() {
+      await assertRevert(
+        this.basil.setToken(this.token.address, {from: this.owner})
+      );
+    });
+
+    it('is the token set in the donations contract', async function() {
+      (await this.basil.token()).should.be.eq(this.token.address);
+    });
+
+  });
+
+  describe('donate', function() {
+
+    // TODO: test highest donation
+
+    // describe('when receiving a donation that is greater than zero', function() {
+
+    //   beforeEach(async function() {
+    //     this.donationValue = 1;
+    //     this.donation = {from: this.donor1, value: web3.toWei(this.donationValue, 'ether')};
+    //     await this.basil.donate(this.donation);
+    //   });
+
+    //   it('increments token id', async function() {
+    //     (await this.basil.numEmittedTokens()).toNumber().should.be.eq(this.donationValue);
+    //   });
+
+    //   it('mints tokens', async function() {
+    //     (await this.token.balanceOf(this.donor1)).toNumber().should.be.eq(this.donationValue);
+    //   });
+
+    // });
+
+  });
+}

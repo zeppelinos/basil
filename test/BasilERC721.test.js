1b7e61157dad22ecb708c7a55ceadc9c7048b20f
Alejandro Santander <Palebluedot@gmail.com>
2018-04-27 12:44:18 -0300

    Disabled non-basic tests and extracted Basil behavior

--- test/BasilERC721.test.js
+++ test/BasilERC721.test.js
@@ -1,48 +1,48 @@
-"use strict";
+// "use strict";
 
-const ERC721Token = artifacts.require('ERC721Token');
-const BasilERC721 = artifacts.require('BasilERC721');
+// const ERC721Token = artifacts.require('ERC721Token');
+// const BasilERC721 = artifacts.require('BasilERC721');
 
-import Deployer from 'kernel/deploy/objects/Deployer';
-import shouldBehaveLikeBasil from './Basil.test';
+// import Deployer from 'kernel/deploy/objects/Deployer';
+// import shouldBehaveLikeBasil from './Basil.behavior';
 
-const ZOS_ADDRESS = "0x212fbf392206bca0a478b9ed3253b08559b35903";
-const ZEPPELIN_VERSION = '1.8.0';
-const ZEPPELIN_DISTRO = 'ZeppelinOS';
-const ERC721_NAME = 'ERC721Token';
+// const ZOS_ADDRESS = "0x212fbf392206bca0a478b9ed3253b08559b35903";
+// const ZEPPELIN_VERSION = '1.8.0';
+// const ZEPPELIN_DISTRO = 'ZeppelinOS';
+// const ERC721_NAME = 'ERC721Token';
 
-contract('BasilERC721', (accounts) => {
+// contract('BasilERC721', (accounts) => {
 
-  const owner = accounts[2];
+//   const owner = accounts[2];
 
-  describe.only('implementation', function() {
+//   describe.only('implementation', function() {
 
-    shouldBehaveLikeBasil(BasilERC721, accounts);
+//     shouldBehaveLikeBasil(BasilERC721, accounts);
 
-    beforeEach(async function () {
+//     beforeEach(async function () {
 
-      // Deploy BasilERC721 implementation.
-      this.basil = await BasilERC721.new();
-      await this.basil.initialize(owner);
+//       // Deploy BasilERC721 implementation.
+//       this.basil = await BasilERC721.new();
+//       await this.basil.initialize(owner);
 
-      // Get a proxy for ZOS' ERC721Token implementation.
-      const controller = await Deployer.projectController(owner, 'TheBasil', ZOS_ADDRESS);
-      const erc721Proxy = await Deployer.createProxy(
-        controller,
-        owner,
-        ERC721Token,
-        ERC721_NAME,
-        ZEPPELIN_DISTRO,
-        ZEPPELIN_VERSION
-      );
+//       // Get a proxy for ZOS' ERC721Token implementation.
+//       const controller = await Deployer.projectController(owner, 'TheBasil', ZOS_ADDRESS);
+//       const erc721Proxy = await Deployer.createProxy(
+//         controller,
+//         owner,
+//         ERC721Token,
+//         ERC721_NAME,
+//         ZEPPELIN_DISTRO,
+//         ZEPPELIN_VERSION
+//       );
 
-      // Set the token in Basil.
-      await this.basil.setToken(erc721Proxy.address);
-    });
+//       // Set the token in Basil.
+//       await this.basil.setToken(erc721Proxy.address);
+//     });
 
-    it('should properly have its ERC721Token set', async function() {
-      const token = await this.basil.token();
-      assert.notEqual(token, 0x0);
-    });
-  });
-});
+//     it('should properly have its ERC721Token set', async function() {
+//       const token = await this.basil.token();
+//       assert.notEqual(token, 0x0);
+//     });
+//   });
+// });
--- test/ZOS.test.js
+++ test/ZOS.test.js
@@ -1,89 +1,89 @@
-'use strict';
+// 'use strict';
 
-import Deployer from 'kernel/deploy/objects/Deployer';
+// import Deployer from 'kernel/deploy/objects/Deployer';
 
-const Basil = artifacts.require('Basil');
-const BasilTestUpgrade = artifacts.require('BasilTestUpgrade.sol');
-const ProjectController = artifacts.require('zos-core/contracts/ProjectController');
-const OwnedUpgradeabilityProxy = artifacts.require("zos-core/contracts/upgradeability/OwnedUpgradeabilityProxy.sol");
-const ZepCore = artifacts.require('kernel/contracts/ZepCore.sol');
+// const Basil = artifacts.require('Basil');
+// const BasilTestUpgrade = artifacts.require('BasilTestUpgrade.sol');
+// const ProjectController = artifacts.require('zos-core/contracts/ProjectController');
+// const OwnedUpgradeabilityProxy = artifacts.require("zos-core/contracts/upgradeability/OwnedUpgradeabilityProxy.sol");
+// const ZepCore = artifacts.require('kernel/contracts/ZepCore.sol');
 
-const ZOS_ADDRESS = "0x212fbf392206bca0a478b9ed3253b08559b35903";
-const ZEPPELIN_VERSION = '1.8.0';
-const ZEPPELIN_DISTRO = 'ZeppelinOS';
+// const ZOS_ADDRESS = "0x212fbf392206bca0a478b9ed3253b08559b35903";
+// const ZEPPELIN_VERSION = '1.8.0';
+// const ZEPPELIN_DISTRO = 'ZeppelinOS';
 
-contract('ZOS', ([_, proxyOwner, owner, aWallet, someone, anotherone]) => {
+// contract('ZOS', ([_, proxyOwner, owner, aWallet, someone, anotherone]) => {
 
-  describe('ProjectController', function() {
+//   describe('ProjectController', function() {
 
-    const projectName = 'TheBasilProject';
-    const contractName = 'Basil';
+//     const projectName = 'TheBasilProject';
+//     const contractName = 'Basil';
 
-    before(async function () {
-      this.controller = await Deployer.projectController(proxyOwner, projectName, ZOS_ADDRESS);
-    });
+//     before(async function () {
+//       this.controller = await Deployer.projectController(proxyOwner, projectName, ZOS_ADDRESS);
+//     });
 
-    describe('controller', function() {
+//     describe('controller', function() {
 
-      it('has a valid fallback provider set', async function() {
-        const provider = await this.controller.fallbackProvider();
-        assert.equal(provider, ZOS_ADDRESS);
-      });
-    });
+//       it('has a valid fallback provider set', async function() {
+//         const provider = await this.controller.fallbackProvider();
+//         assert.equal(provider, ZOS_ADDRESS);
+//       });
+//     });
 
-    describe('ZepCore', function() {
+//     describe('ZepCore', function() {
 
-      it('has an implementation for ERC721Token', async function() {
-        const core = ZepCore.at(ZOS_ADDRESS);
-        const impl = await core.getImplementation(ZEPPELIN_DISTRO, ZEPPELIN_VERSION, 'ERC721Token');
-        assert.notEqual(impl, 0x0);
-      });
-    });
+//       it('has an implementation for ERC721Token', async function() {
+//         const core = ZepCore.at(ZOS_ADDRESS);
+//         const impl = await core.getImplementation(ZEPPELIN_DISTRO, ZEPPELIN_VERSION, 'ERC721Token');
+//         assert.notEqual(impl, 0x0);
+//       });
+//     });
 
-    describe('registry', function() {
+//     describe('registry', function() {
 
-      it('can add implementation 0', async function() {
-        this.basilImplementation0 = await Deployer.deployAndRegister(this.controller, Basil, contractName, '0');
-      });
+//       it('can add implementation 0', async function() {
+//         this.basilImplementation0 = await Deployer.deployAndRegister(this.controller, Basil, contractName, '0');
+//       });
 
-      it('can add implementation 1', async function() {
-        this.basilImplementation1 = await Deployer.deployAndRegister(this.controller, BasilTestUpgrade, contractName, '1');
-      });
+//       it('can add implementation 1', async function() {
+//         this.basilImplementation1 = await Deployer.deployAndRegister(this.controller, BasilTestUpgrade, contractName, '1');
+//       });
 
-      describe('controller with registered implementations', function() {
+//       describe('controller with registered implementations', function() {
 
-        it('knows of implementation 0', async function() {
-          const controllerImplementation = await this.controller.getImplementation(projectName, '0', contractName);
-          assert.equal(controllerImplementation, this.basilImplementation0.address);
-        });
+//         it('knows of implementation 0', async function() {
+//           const controllerImplementation = await this.controller.getImplementation(projectName, '0', contractName);
+//           assert.equal(controllerImplementation, this.basilImplementation0.address);
+//         });
 
-        it('knows of implementation 1', async function() {
-          const controllerImplementation = await this.controller.getImplementation(projectName, '1', contractName);
-          assert.equal(controllerImplementation, this.basilImplementation1.address);
-        });
+//         it('knows of implementation 1', async function() {
+//           const controllerImplementation = await this.controller.getImplementation(projectName, '1', contractName);
+//           assert.equal(controllerImplementation, this.basilImplementation1.address);
+//         });
 
-        it('can create a proxy for implementation 0', async function() {
-          this.basilProxy = await Deployer.createProxyAndCall(
-            this.controller,
-            proxyOwner,
-            Basil,
-            contractName,
-            projectName,
-            '0',
-            ['address'],
-            proxyOwner
-          );
-        });
+//         it('can create a proxy for implementation 0', async function() {
+//           this.basilProxy = await Deployer.createProxyAndCall(
+//             this.controller,
+//             proxyOwner,
+//             Basil,
+//             contractName,
+//             projectName,
+//             '0',
+//             ['address'],
+//             proxyOwner
+//           );
+//         });
 
-        it('can upgrade the proxy to implementation 1', async function() {
-          await this.controller.upgradeTo(this.basilProxy.address, projectName, '1', contractName, {from: proxyOwner});
-          const proxy = OwnedUpgradeabilityProxy.at(this.basilProxy.address);
-          assert.equal(await proxy.implementation(), this.basilImplementation1.address);
-          const basil_v1 = await BasilTestUpgrade.at(this.basilProxy.address);
-          const msg = await basil_v1.sayHi();
-          assert.equal(msg, "Hi!");
-        });
-      });
-    });
-  })
-})
+//         it('can upgrade the proxy to implementation 1', async function() {
+//           await this.controller.upgradeTo(this.basilProxy.address, projectName, '1', contractName, {from: proxyOwner});
+//           const proxy = OwnedUpgradeabilityProxy.at(this.basilProxy.address);
+//           assert.equal(await proxy.implementation(), this.basilImplementation1.address);
+//           const basil_v1 = await BasilTestUpgrade.at(this.basilProxy.address);
+//           const msg = await basil_v1.sayHi();
+//           assert.equal(msg, "Hi!");
+//         });
+//       });
+//     });
+//   })
+// })

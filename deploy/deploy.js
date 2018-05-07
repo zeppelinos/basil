fc56792b55a2a55b6a4bca0ee172c644a2c3e19f
ajs <palebluedot@gmail.com>
2018-05-07 15:31:44 -0300

    Unhook zos temporarily

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -1,117 +1,117 @@
-import DeployData from './deploy_data_util.js';
-import Deployer from 'kernel/deploy/objects/Deployer';
+// import DeployData from './deploy_data_util.js';
+// import Deployer from 'kernel/deploy/objects/Deployer';
 
-const Basil = artifacts.require("./Basil.sol");
-const BasilERC721 = artifacts.require("./BasilERC721.sol");
-const ProjectController = artifacts.require('ProjectController');
+// const Basil = artifacts.require("./Basil.sol");
+// const BasilERC721 = artifacts.require("./BasilERC721.sol");
+// const ProjectController = artifacts.require('ProjectController');
 
-const PROJECT_OWNER = web3.eth.accounts[0];
-const PROJECT_NAME = 'TheBasil';
-const BASIL_CONTRACT_NAME = 'Basil';
+// const PROJECT_OWNER = web3.eth.accounts[0];
+// const PROJECT_NAME = 'TheBasil';
+// const BASIL_CONTRACT_NAME = 'Basil';
 
-let data;
-let controller;
-let basilProxy;
+// let data;
+// let controller;
+// let basilProxy;
 
-const FORCE_RE_DEPLOY_ON_DEVELOPMENT = true;
-const ZOS_ADDRESS = {
-  development: 0x212fbf392206bca0a478b9ed3253b08559b35903,
-  ropsten: 0x0
-};
+// const FORCE_RE_DEPLOY_ON_DEVELOPMENT = true;
+// const ZOS_ADDRESS = {
+//   development: 0x212fbf392206bca0a478b9ed3253b08559b35903,
+//   ropsten: 0x0
+// };
 
-async function deploy() {
-  data = DeployData.read(network);
-  await deployController();
-  await deployBasil();
-  await deployBasilERC721();
-}
+// async function deploy() {
+//   data = DeployData.read(network);
+//   await deployController();
+//   await deployBasil();
+//   await deployBasilERC721();
+// }
 
-async function deployVersion(version, contractName, ContractKlazz) {
-  console.log(`deploying and registering version ${version} of ${contractName}...`);
-  const implementation = await Deployer.deployAndRegister(controller, ContractKlazz, contractName, version);
-  console.log(`implementation deployed, version: ${version}, at: ${implementation.address}`);
-  return implementation;
-}
+// async function deployVersion(version, contractName, ContractKlazz) {
+//   console.log(`deploying and registering version ${version} of ${contractName}...`);
+//   const implementation = await Deployer.deployAndRegister(controller, ContractKlazz, contractName, version);
+//   console.log(`implementation deployed, version: ${version}, at: ${implementation.address}`);
+//   return implementation;
+// }
 
-function forceReDeploy() {
-  return FORCE_RE_DEPLOY_ON_DEVELOPMENT && network === "development";
-}
+// function forceReDeploy() {
+//   return FORCE_RE_DEPLOY_ON_DEVELOPMENT && network === "development";
+// }
 
-async function deployBasil() {
-  const version = '0';
-  if(forceReDeploy() || !data.contracts || !data.contracts[BASIL_CONTRACT_NAME]) {
+// async function deployBasil() {
+//   const version = '0';
+//   if(forceReDeploy() || !data.contracts || !data.contracts[BASIL_CONTRACT_NAME]) {
 
-    // Deploy and register implementation.
-    const implementation = await deployVersion(version, BASIL_CONTRACT_NAME, Basil);
+//     // Deploy and register implementation.
+//     const implementation = await deployVersion(version, BASIL_CONTRACT_NAME, Basil);
 
-    // Create proxy with first implementation.
-    console.log(`creating proxy for ${BASIL_CONTRACT_NAME}...`);
-    basilProxy = await Deployer.createProxyAndCall(
-      controller,
-      PROJECT_OWNER,
-      Basil,
-      BASIL_CONTRACT_NAME,
-      PROJECT_NAME,
-      version,
-      ['address'],
-      [PROJECT_OWNER]
-    );
-    console.log(`deployed proxy: ${basilProxy.address}`);
+//     // Create proxy with first implementation.
+//     console.log(`creating proxy for ${BASIL_CONTRACT_NAME}...`);
+//     basilProxy = await Deployer.createProxyAndCall(
+//       controller,
+//       PROJECT_OWNER,
+//       Basil,
+//       BASIL_CONTRACT_NAME,
+//       PROJECT_NAME,
+//       version,
+//       ['address'],
+//       [PROJECT_OWNER]
+//     );
+//     console.log(`deployed proxy: ${basilProxy.address}`);
 
-    // Save to disk.
-    data = DeployData.saveContractProxy(data, BASIL_CONTRACT_NAME, basilProxy.address, network);
-    data = DeployData.appendContractVersion(data, BASIL_CONTRACT_NAME, version, implementation.address, network);
-  }
-  else {
-    // TODO: the fact that the version is not found in the json does not necessarily mean it is not
-    // in the registry, which probably needs to be accounted for.
-    console.log('found Basil version 0, no need to deploy it.');
-  }
-}
+//     // Save to disk.
+//     data = DeployData.saveContractProxy(data, BASIL_CONTRACT_NAME, basilProxy.address, network);
+//     data = DeployData.appendContractVersion(data, BASIL_CONTRACT_NAME, version, implementation.address, network);
+//   }
+//   else {
+//     // TODO: the fact that the version is not found in the json does not necessarily mean it is not
+//     // in the registry, which probably needs to be accounted for.
+//     console.log('found Basil version 0, no need to deploy it.');
+//   }
+// }
 
-async function deployBasilERC721() {
-  const version = '1';
-  if(forceReDeploy() || !data.contracts.Basil.versions[version]) {
+// async function deployBasilERC721() {
+//   const version = '1';
+//   if(forceReDeploy() || !data.contracts.Basil.versions[version]) {
 
-    // Deploy and register implementation.
-    const implementation = await deployVersion(version, BASIL_CONTRACT_NAME, BasilERC721);
+//     // Deploy and register implementation.
+//     const implementation = await deployVersion(version, BASIL_CONTRACT_NAME, BasilERC721);
 
-    // Upgrade proxy.
-    controller.upgradeTo(basilProxy.address, PROJECT_NAME, version, BASIL_CONTRACT_NAME);
-    console.log(`upgraded ${BASIL_CONTRACT_NAME} proxy to version ${version}`);
+//     // Upgrade proxy.
+//     controller.upgradeTo(basilProxy.address, PROJECT_NAME, version, BASIL_CONTRACT_NAME);
+//     console.log(`upgraded ${BASIL_CONTRACT_NAME} proxy to version ${version}`);
 
-    // Save to disk.
-    DeployData.appendContractVersion(data, BASIL_CONTRACT_NAME, version, implementation.address, network);
-  }
-  else {
-    console.log('found Basil version 1, no need to deploy it.');
-  }
-}
+//     // Save to disk.
+//     DeployData.appendContractVersion(data, BASIL_CONTRACT_NAME, version, implementation.address, network);
+//   }
+//   else {
+//     console.log('found Basil version 1, no need to deploy it.');
+//   }
+// }
 
-async function deployController() {
-  if(forceReDeploy() || !data.controllerAddress) {
+// async function deployController() {
+//   if(forceReDeploy() || !data.controllerAddress) {
 
-    // Deploy a new project controller.
-    console.log(`did not find a project controller, deploying a new one...`);
-    controller = await Deployer.projectController(
-      PROJECT_OWNER,
-      PROJECT_NAME,
-      ZOS_ADDRESS[network]
-    );
-    console.log(`deployed new project controller: ${controller.address}`);
+//     // Deploy a new project controller.
+//     console.log(`did not find a project controller, deploying a new one...`);
+//     controller = await Deployer.projectController(
+//       PROJECT_OWNER,
+//       PROJECT_NAME,
+//       ZOS_ADDRESS[network]
+//     );
+//     console.log(`deployed new project controller: ${controller.address}`);
 
-    // Save to disk.
-    data = DeployData.saveController(data, network, controller.address);
-  }
-  else {
+//     // Save to disk.
+//     data = DeployData.saveController(data, network, controller.address);
+//   }
+//   else {
 
-    // Retrieve project controller.
-    console.log(`found project controller, reusing it...`);
-    controller = await ProjectController.at(data.controllerAddress);
-    console.log(`retrieved project controller: ${controller.address}`);
-  }
-}
+//     // Retrieve project controller.
+//     console.log(`found project controller, reusing it...`);
+//     controller = await ProjectController.at(data.controllerAddress);
+//     console.log(`retrieved project controller: ${controller.address}`);
+//   }
+// }
 
-module.exports = function(cb) {
-  deploy().then(cb).catch(cb);
-}
+// module.exports = function(cb) {
+//   deploy().then(cb).catch(cb);
+// }
--- deploy/deploy_data_util.js
+++ deploy/deploy_data_util.js
@@ -1,62 +1,62 @@
-const fs = require('fs');
+// const fs = require('fs');
 
-const DeployData = {
+// const DeployData = {
 
-  deployPath(network) {
-    return `./deploy/deploy_data.${network}.json`;
-  },
+//   deployPath(network) {
+//     return `./deploy/deploy_data.${network}.json`;
+//   },
 
-  read(network) {
-    let data;
-    const path = this.deployPath(network);
-    try {
-      const raw = fs.readFileSync(path, 'utf8');
-      data = JSON.parse(raw);
-    }
-    catch(err) {
-      data = {
-        controllerAddress: undefined,
-        contracts: {}
-      };
-    };
-    return data;
-  },
+//   read(network) {
+//     let data;
+//     const path = this.deployPath(network);
+//     try {
+//       const raw = fs.readFileSync(path, 'utf8');
+//       data = JSON.parse(raw);
+//     }
+//     catch(err) {
+//       data = {
+//         controllerAddress: undefined,
+//         contracts: {}
+//       };
+//     };
+//     return data;
+//   },
 
-  write(data, network) {
-    const writeData = JSON.stringify(data, null, 2);
-    const path = this.deployPath(network);
-    fs.writeFileSync(path, writeData, 'utf8');
-  },
+//   write(data, network) {
+//     const writeData = JSON.stringify(data, null, 2);
+//     const path = this.deployPath(network);
+//     fs.writeFileSync(path, writeData, 'utf8');
+//   },
 
-  saveController(data, network, controllerAddress) {
-    data.controllerAddress = controllerAddress;
-    this.write(data, network);
-    return data;
-  },
+//   saveController(data, network, controllerAddress) {
+//     data.controllerAddress = controllerAddress;
+//     this.write(data, network);
+//     return data;
+//   },
 
-  saveContractProxy(data, contractName, proxyAddress, network) {
-    data = this.prepareContractEntry(data, contractName);
-    data.contracts[contractName].proxyAddress = proxyAddress;
-    this.write(data, network);
-    return data;
-  },
+//   saveContractProxy(data, contractName, proxyAddress, network) {
+//     data = this.prepareContractEntry(data, contractName);
+//     data.contracts[contractName].proxyAddress = proxyAddress;
+//     this.write(data, network);
+//     return data;
+//   },
 
-  appendContractVersion(data, contractName, version, implementation, network) {
-    data = this.prepareContractEntry(data, contractName);
-    data.contracts[contractName].versions[version] = implementation;
-    this.write(data, network);
-    return data;
-  },
+//   appendContractVersion(data, contractName, version, implementation, network) {
+//     data = this.prepareContractEntry(data, contractName);
+//     data.contracts[contractName].versions[version] = implementation;
+//     this.write(data, network);
+//     return data;
+//   },
 
-  prepareContractEntry(data, contractName) {
-    if(!data.contracts) data.contracts = {};
-    if(data.contracts[contractName]) return data;
-    data.contracts[contractName] = {
-      proxyAddress: undefined,
-      versions: {}
-    };
-    return data;
-  }
-}
+//   prepareContractEntry(data, contractName) {
+//     if(!data.contracts) data.contracts = {};
+//     if(data.contracts[contractName]) return data;
+//     data.contracts[contractName] = {
+//       proxyAddress: undefined,
+//       versions: {}
+//     };
+//     return data;
+//   }
+// }
 
-export default DeployData;
+// export default DeployData;

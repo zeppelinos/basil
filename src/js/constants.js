350e37236b30e84159876a289018a8dbbd9774c6
ajs <palebluedot@gmail.com>
2018-05-07 15:45:11 -0300

    Use deployed basil address for now

--- src/js/constants.js
+++ src/js/constants.js
@@ -2,5 +2,6 @@ import * as config from '../../truffle.js';
 
 export const ACTIVE_NETWORK = config.networks.development;
 
-const deployData = require(`../../deploy/deploy_data.${ACTIVE_NETWORK.name}.json`);
-export const BASIL_ADDRESS = deployData.contracts.Basil.proxyAddress;
+// const deployData = require(`../../deploy/deploy_data.${ACTIVE_NETWORK.name}.json`);
+// export const BASIL_ADDRESS = deployData.contracts.Basil.proxyAddress;
+export const BASIL_ADDRESS = "0x4103dee0e21ad2d2181a887034fe52fe446f7f25";

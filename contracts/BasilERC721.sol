302f66736e9608985f54e74676c16be54c656b3a
ajs <palebluedot@gmail.com>
2018-05-07 16:38:28 -0300

    Temporarily disable BasilERC721.sol and all tests

--- contracts/BasilERC721.sol
+++ contracts/BasilERC721.sol
@@ -1,27 +1,27 @@
-pragma solidity ^0.4.21;
+// pragma solidity ^0.4.21;
 
-import "./Basil.sol";
-import "kernel/contracts/test/kernel_instance/ERC721Token.sol";
+// import "./Basil.sol";
+// import "kernel/contracts/test/kernel_instance/ERC721Token.sol";
 
-contract BasilERC721 is Basil {
+// contract BasilERC721 is Basil {
 
-  ERC721Token public token;
-  uint256 public numEmittedTokens;
+//   ERC721Token public token;
+//   uint256 public numEmittedTokens;
 
-  function setToken(address _erc721Address) external onlyOwner {
-    require(_erc721Address != address(0));
-    require(address(token) == address(0));
-    token = ERC721Token(_erc721Address);
-    token.initialize();
-  }
+//   function setToken(address _erc721Address) external onlyOwner {
+//     require(_erc721Address != address(0));
+//     require(address(token) == address(0));
+//     token = ERC721Token(_erc721Address);
+//     token.initialize();
+//   }
 
-  function donate(uint256 _r, uint256 _g, uint256 _b) public payable {
-    super.donate(_r, _g, _b);
-    emitUniqueToken(tx.origin);
-  }
+//   function donate(uint256 _r, uint256 _g, uint256 _b) public payable {
+//     super.donate(_r, _g, _b);
+//     emitUniqueToken(tx.origin);
+//   }
 
-  function emitUniqueToken(address _tokenOwner) internal {
-    token.mint(_tokenOwner, numEmittedTokens);
-    numEmittedTokens++;
-  }
-}
+//   function emitUniqueToken(address _tokenOwner) internal {
+//     token.mint(_tokenOwner, numEmittedTokens);
+//     numEmittedTokens++;
+//   }
+// }

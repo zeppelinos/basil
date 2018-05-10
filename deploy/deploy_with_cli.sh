1c023611808d388ddf183dc6994f64a71cbb2d93
ajs <palebluedot@gmail.com>
2018-05-10 19:30:36 -0300

    Minor updates to the deploy script

--- deploy/deploy_with_cli.sh
+++ deploy/deploy_with_cli.sh
@@ -27,7 +27,7 @@ rm -f package.zos.json
 rm -f package.zos.$NETWORK.json
 
 # Compile all contracts.
-npx truffle compile
+rm -rf build && npx truffle compile
 
 # Initialize project
 # NOTE: Creates a package.zos.json file that keeps track of the project's details
@@ -65,21 +65,20 @@ zos sync --network $NETWORK
 
 # Create a proxy for the standard library's ERC721 token.
 BASIL=$(jq ".proxies.Basil[0].address" package.zos.$NETWORK.json --raw-output)
-# BASIL=$(jq ".proxies.Basil[0].address" package.zos.$NETWORK.json)
-"Using Basil proxy deployed at: "$BASIL
+echo "Using Basil proxy deployed at: "$BASIL
 zos create-proxy MintableERC721Token --from $OWNER --init --args $BASIL,BasilToken,BSL --network $NETWORK
 
 # Read deployed addresses
 ERC721=$(jq ".proxies.MintableERC721Token[0].address" package.zos.$NETWORK.json)
-"Token deployed at: "$ERC721
+echo "Token deployed at: "$ERC721
 
 # Upgrade the existing contract proxy to use the new version
-zos upgrade-proxy Basil null --network $NETWORK
+zos upgrade-proxy Basil --all --network $NETWORK
 
 # Register the token in Basil.
-"Registering token in Basil..."
-"BasilERC721.at(\"$BASIL\").setToken($ERC721, {from: \"$OWNER\"})" | truffle console --network $NETWORK
-exit
+echo "Registering token in Basil..."
+echo "BasilERC721.at(\"$BASIL\").setToken($ERC721, {from: \"$OWNER\"})" | truffle console --network $NETWORK
+# exit
 
 # Disable command logging
 set +x

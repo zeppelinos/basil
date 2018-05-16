f6e8f50e7f5c1d5312f2e5eb5ebe011c556434bf
Martín Triay <martriay@gmail.com>
2018-05-16 16:12:49 -0300

    Iterate donations list and navbar styles

--- src/js/components/DonateForm.react.js
+++ src/js/components/DonateForm.react.js
@@ -21,12 +21,11 @@ class DonateForm extends React.Component {
     const rgb = this.state.color.rgb;
     const { address, balance } = this.props.account;
     return (
-      <div className="col">
-        <form className="basil card" onSubmit={this._handleSubmit}>
-          <div className="card-content">
-            <div className="row no-margin">
-              <div className="donate-form col s6">
-
+      <form className="basil card" onSubmit={this._handleSubmit}>
+        <div className="card-content">
+          <div className="row no-margin">
+            <div className="col s6">
+              <div className="donate-form">
                 <h3 className="title">Customize the Zeppelin LED</h3>
 
                 <HuePicker className="color-picker" style={{width: 100}} onChangeComplete={this._updateColor} color={rgb}/>
@@ -43,16 +42,16 @@ class DonateForm extends React.Component {
                   </div>
                 </div>
               </div>
-
-              <div className="basil-icon col s6" style={{backgroundColor: `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`}}>
-                <img width="150" src={require('../../images/basil.svg')}></img>
-                <span className="notch"></span>
-              </div>
-
             </div>
+
+            <div className="basil-icon col s6" style={{backgroundColor: `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`}}>
+              <img width="150" src={require('../../images/basil.svg')}></img>
+              <span className="notch"></span>
+            </div>
+
           </div>
-        </form>
-      </div>
+        </div>
+      </form>
     )
   }
 
--- src/js/components/Navbar.react.js
+++ src/js/components/Navbar.react.js
@@ -13,19 +13,22 @@ class Navbar extends React.Component {
   render() {
     const basil = this.props.basil
     return (
-      <nav className="main-nav">
-        <div className="nav-wrapper">
-          <Link to="/" className="brand-logo">
-            <img width="150" src={require('../../images/zeppelin-logo.png')}/>
-            pi
-          </Link>
-          {basil &&
-            <ul className="right grey-text">
-              <li>Last donation: <strong>{basil.highestDonation} ETH</strong></li>
-              &nbsp;
-              <li>RGB: <strong>{basil.r} - {basil.g} - {basil.b}</strong></li>
-            </ul>
-          }
+      <nav className="container main-nav">
+        <div className="row nav-wrapper">
+          <div className="col m12 l6">
+            <Link to="/" className="brand-logo">
+              <img width="150" src={require('../../images/zeppelin-logo.png')}/>
+            </Link>
+          </div>
+          <div className="col m12 l6">
+            {basil &&
+              <ul className="right grey-text">
+                <li>Last donation: <strong>{basil.highestDonation} ETH</strong></li>
+                &nbsp;
+                <li>RGB: <strong>{basil.r} - {basil.g} - {basil.b}</strong></li>
+              </ul>
+            }
+          </div>
         </div>
       </nav>
     )

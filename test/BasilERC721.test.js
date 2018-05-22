b533ae693b0077371ae42f19f1110d27563a8e59
ajs <palebluedot@gmail.com>
2018-05-22 17:49:32 -0300

    Fix tests

--- test/BasilERC721.test.js
+++ test/BasilERC721.test.js
@@ -5,7 +5,7 @@ const MintableERC721Token = artifacts.require('MintableERC721Token');
 
 const shouldBehaveLikeBasilWithTokens = require('./BasilWithTokens.behavior.js');
 
-contract.only('BasilERC721', ([_, owner, aWallet, someone, anotherone]) => {
+contract('BasilERC721', ([_, owner, aWallet, someone, anotherone]) => {
 
   const tokenName = 'BSLToken';
   const tokenSymbol = 'BSL';
@@ -18,13 +18,14 @@ contract.only('BasilERC721', ([_, owner, aWallet, someone, anotherone]) => {
     this.token = await MintableERC721Token.new();
     // Note: truffle can't handle function overloading here, so 
     // we're using zos instead to make the call to initialize
-    // wait this.token.initialize(this.basil.address, tokenName, tokenSymbol, {});
+    // wait this.token.initialize(this.basil.address, tokenName, tokenSymbol);
     const data = encodeCall(
       "initialize", 
       ['address', 'string', 'string'],
       [this.basil.address, tokenName, tokenSymbol]
     );
-    await this.token.sendTransaction({data});
+    await this.token.sendTransaction({data, from: owner});
+    await this.basil.setToken(this.token.address, {from: owner});
   });
 
   shouldBehaveLikeBasilWithTokens(owner, aWallet, someone, anotherone, tokenName, tokenSymbol);

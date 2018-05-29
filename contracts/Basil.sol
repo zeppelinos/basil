d4f62ce3b8bc881efe525cba49c089cf382dedda
Robert Oschler <roschler@users.noreply.github.com>
2018-05-29 11:37:51 -0400

    Eliminated truffle compile warnings.
    The original version triggered several warnings from the Truffle compiler related to using address types and emitting Events.  This version eliminates those warnings.

--- contracts/Basil.sol
+++ contracts/Basil.sol
@@ -1,44 +1,46 @@
-pragma solidity ^0.4.21;
-
-import "openzeppelin-zos/contracts/ownership/Ownable.sol";
-
 /**
  * @title Basil
  */
+ 
+pragma solidity ^0.4.21;
+ 
+ 
+import "../node_modules/openzeppelin-zos/contracts/ownership/Ownable.sol";
+
 contract Basil is Ownable {
-
-  // color
-  uint256 public r;
-  uint256 public g;
-  uint256 public b;
-
-  // highest donation in wei
-  uint256 public highestDonation;
-
-  event Withdrawal(address indexed wallet, uint256 value);
-  event NewDonation(address indexed donor, uint256 value, uint256 r, uint256 g, uint256 b);
-
-  function donate(uint256 _r, uint256 _g, uint256 _b) public payable {
-    require(_r < 256);
-    require(_g < 256);
-    require(_b < 256);
-    require(msg.value > highestDonation);
-
-    r = _r;
-    g = _g;
-    b = _b;
-    highestDonation = msg.value;
-    NewDonation(
-      msg.sender, msg.value,
-      r, g, b);
-  }
-
-  function withdraw(address wallet) public onlyOwner {
-    require(this.balance > 0);
-    require(wallet != address(0));
-    uint256 value = this.balance;
-    wallet.transfer(value);
-    Withdrawal(wallet, value);
-  }
-
+    
+    // color
+    uint256 public r;
+    uint256 public g;
+    uint256 public b;
+    
+    // highest donation in wei
+    uint256 public highestDonation;
+    
+    event Withdrawal(address indexed wallet, uint256 value);
+    event NewDonation(address indexed donor, uint256 value, uint256 r, uint256 g, uint256 b);
+    
+    function donate(uint256 _r, uint256 _g, uint256 _b) public payable {
+        require(_r < 256);
+        require(_g < 256);
+        require(_b < 256);
+        
+        r = _r;
+        g = _g;
+        b = _b;
+        
+        // Donation must be higher than the current highest
+        require(msg.value > highestDonation);
+        
+        emit NewDonation(msg.sender, msg.value, r, g, b);
+    }
+    
+    function withdraw(address wallet) public onlyOwner {
+        require(address(this).balance > 0);
+        require(wallet != address(0));
+        uint256 value = address(this).balance;
+        wallet.transfer(value);
+        emit Withdrawal(wallet, value);
+    }    
+    
 }
